26d630ed7e507ef17e5109bf4ad41038cac9a58e
teja varma alluri <153795647+teja1021@users.noreply.github.com>
2025-06-09 16:24:43 +0530

    header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -25,7 +25,7 @@ export default async function Header() {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2 cursor-pointer">
           <Image
-            src="/logo-single.png"
+            src="/logo.png"
             alt="Medimeet Logo"
             width={200}
             height={60}
